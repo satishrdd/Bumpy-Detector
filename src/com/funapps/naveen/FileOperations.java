8bb53dee0d28fa33640f123d7bb438b211e78c51
naveen439 <chedetinaveen@gmail.com>
2015-01-15 23:29:01 +0530

    Added Files to store data in Internal Storage

---
+++ src/com/funapps/naveen/FileOperations.java
@@ -0,0 +1,49 @@
+package com.funapps.naveen;
+
+import java.io.BufferedReader;
+import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.OutputStreamWriter;
+
+import android.util.Log;
+
+public class FileOperations {
+	public FileOperations() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public boolean write(String filename, String filecontent) {
+		try {
+			String filepath = "/sdcard/" + filename + ".txt";
+			FileOutputStream fos = new FileOutputStream(filepath, true);
+			OutputStreamWriter osw = new OutputStreamWriter(fos);
+			osw.write(filecontent+"\n");
+			osw.close();
+			Log.d("Success", "Success");
+
+		} catch (Exception e) {
+			Log.d("Error", "Can't write to file");
+		}
+
+		return false;
+
+	}
+	public String read(String filename){
+		
+		String Response = null;
+		try{
+			String filepath = "/sdcard/"+filename+".txt";
+			StringBuffer sb = new StringBuffer();
+			BufferedReader br = new BufferedReader(new FileReader(filepath));
+			String line = "";
+			while((line = br.readLine())!=null){
+				sb.append(line+"\n");
+			}
+			Response = sb.toString();
+			br.close();
+		}catch(Exception e){
+			Log.d("Exception","Can't read file");
+		}
+		return Response;
+	}
+}
